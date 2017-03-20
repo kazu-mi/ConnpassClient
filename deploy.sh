6856a8f8c9948b028f424774f80bbf5583490080
kazuumi <kazuyuka76@gmail.com>
2017-03-20 20:22:19 +0900

    もういっかい

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ./gradlew assembleDebug
-curl -F "token=${DEPLOY_GATE_API_KEY}" -F "file=@app/build/outputs/apk/test.apk" -F "message=deployed" https://deploygate.com/api/users/kazuyuka76/apps
+curl -F "token=${DEPLOY_GATE_API_KEY}" -F "file=@app/build/outputs/apk/app-debug.apk" -F "message=deployed" https://deploygate.com/api/users/kazuyuka76/apps
