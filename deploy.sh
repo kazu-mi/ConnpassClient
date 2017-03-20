85d51aa2eb6df112f2fda803e69b3a34caa18049
kazuumi <kazuyuka76@gmail.com>
2017-03-20 18:56:03 +0900

    circle CI 環境設定

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+./gradlew assembleRelease
+curl -F "token=${DEPLOY_GATE_API_KEY}" -F "file=app/build/outputs/apk/app-release-unsigned.apk" -F "message=deployed" https://deploygate.com/api/users/kazuyuka76/apps
